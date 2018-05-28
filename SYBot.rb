71edabcd1eef7ad3ad8473b3da7dd0323ed235ee
yoshida-shu <p8zu5g4d@s.okayama-u.ac.jp>
2018-05-28 14:07:41 +0900

    Add GITHUB_API address to SYBot

--- SYBot.rb
+++ SYBot.rb
@@ -10,7 +10,7 @@ require 'uri'
 require 'yaml'
 require 'net/https'
 
- GITHUB_API = ENV['GITHUB_API'] 
+ GITHUB_API = https://api.github.com/repos/nomlab/nompedia/issues 
  USERNAME = ENV['GIT_USERNAME']
  PASSWORD = ENV['GIT_PASSWORD']
 
