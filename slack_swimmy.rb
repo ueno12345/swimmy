d8c7319f634f9d2fafc88ef0fc4cb9ddeb83dcdd
Yuuki Fujiwara <fujiwara2018@s.okayama-u.ac.jp>
2018-12-10 13:36:21 +0900

    Move files to place them like Gem's directory structure

--- slack_swimmy.rb
+++
@@ -1,7 +0,0 @@
-require 'slack-ruby-bot'
-require 'slack_swimmy/commands/FBot'
-require 'slack_swimmy/commands/AmeBot'
-require 'slack_swimmy/commands/TakaBot'
-require 'slack_swimmy/commands/SYBot'
-require 'slack_swimmy/app'
-require 'slack_swimmy/commands/post_auto'
--- web.rb
+++
@@ -1,9 +0,0 @@
-require 'sinatra/base'
-
-module Swimmy
-  class Web < Sinatra::Base
-    get '/' do
-      "swimmy"
-    end
-  end
-end
