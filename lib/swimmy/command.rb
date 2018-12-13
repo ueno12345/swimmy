aae81e0ef8f95bf20292910770e21c5a93e59b13
Yoshinari Nomura <nom@nomlab.org>
2018-12-13 09:50:31 +0900

    Introduce Sheetq gem

--- lib/swimmy/command.rb
+++ lib/swimmy/command.rb
@@ -1,8 +1,12 @@
-dir = File.dirname(__FILE__) + "/command"
+module Swimmy
+  module Command
+    dir = File.dirname(__FILE__) + "/command"
 
-require "#{dir}/issue_operation"
-require "#{dir}/lunch_time"
-require "#{dir}/rain_information"
-require "#{dir}/restaurant_information"
-require "#{dir}/route"
-require "#{dir}/hide_defalts"
+    require "#{dir}/issue_operation"
+    require "#{dir}/lunch_time"
+    require "#{dir}/rain_information"
+    require "#{dir}/restaurant_information"
+    require "#{dir}/route"
+  # require "#{dir}/hide_defalts"
+  end
+end
