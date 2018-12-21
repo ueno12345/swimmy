952133247d99209a77b6f96ade9bffb9dd083835
Eiji Yamamoto <yamamoto-e@swlab.cs.okayama-u.ac.jp>
2018-12-21 11:38:50 +0900

    Add lottery bot
    lottery randomly assigns numbers to people

--- lib/swimmy/command.rb
+++ lib/swimmy/command.rb
@@ -1,8 +1,11 @@
 dir = File.dirname(__FILE__) + "/command"
 
-require "#{dir}/issue_operation"
-require "#{dir}/lunch_time"
-require "#{dir}/rain_information"
-require "#{dir}/restaurant_information"
-require "#{dir}/route"
-require "#{dir}/hide_defalts"
+    require "#{dir}/lottery"
+    require "#{dir}/issue_operation"
+    require "#{dir}/lunch_time"
+    require "#{dir}/rain_information"
+    require "#{dir}/restaurant_information"
+    require "#{dir}/route"
+    #require "#{dir}/hide_defalts"
+  end
+end
