0900e35c05f4d1225166d700474441453810bbac
Yoshinari Nomura <nom@nomlab.org>
2019-02-23 00:47:29 +0900

    Rewrite QiitaTrend and LunchTime using tick framework

--- lib/swimmy/command/lunch_time.rb
+++ lib/swimmy/command/lunch_time.rb
@@ -1,33 +1,60 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'dotenv'
-Dotenv.load
+module Swimmy
+  module Command
+    class LunchTime < Swimmy::Command::Base
 
-require 'slack-ruby-client'
+      # per-channel post schedule
+      POST_SCHEDULE = {}
 
-post_channel = ENV['POST_CHANNEL']
+      command "lunch_time" do |client, data, match|
+        case match[:expression]
+        when /(\d+):(\d+)/
+          h, m = $1.to_i, $2.to_i
+          unless 0 <= h && h <= 23 && 0 <= m && m <= 59
+            client.say(channel: data.channel, text: "時刻の指定がおかしいです")
+            break
+          end
 
-Slack.configure do |conf|
-  conf.token = ENV["SLACK_API_TOKEN"]
-end
+          now = Time.now
+          nxt = Time.new(now.year, now.month, now.day, h, m)
+          nxt += 86400 if nxt <= now
+          POST_SCHEDULE[data.channel] = nxt
+          client.say(channel: data.channel, text: "<##{data.channel}> に #{h}時#{m}分に通知します．")
 
-client = Slack::RealTime::Client.new
+        when "off"
+          POST_SCHEDULE.delete(data.channel)
+          client.say(channel: data.channel, text: "<##{data.channel}> には通知しません．")
 
-Thread.new do
-  client.on :hello do
-    loop do
-      t = Time.new
-      unless t.sunday? || t.saturday?
-        if t.hour == 11 && t.min == 30
-          client.message channel: post_channel, text: 'そろそろ，お昼ご飯の時間ですよ!!'
+        else
+          client.say(channel: data.channel, text: help_message("lunch_time"))
         end
       end
-      sleep 60
-    end
-  end
 
-  client.on :closed do
-    client.start!
-  end
+      help do
+        title "lunch_time"
+        desc "ランチタイムだねって時にお知らせします．"
+        long_desc "lunch_time HH:MM - 毎日平日 HH:MM 頃，このチャンネルに知らせます．\n" +
+                  "lunch_time off お知らせをやめます．\n"
+      end
 
-  client.start!
-end
+      tick do |client, data|
+        puts "Lunch Time..."
+        now = Time.new
+
+        POST_SCHEDULE.each do |channel, time|
+          puts "Lunch Time #{channel} #{time}"
+
+          if time <= now
+            puts "Lunch Time Sending message..."
+
+            # slide schedule to the next HH:MM
+            POST_SCHEDULE[channel] += ((now - time).to_i / 86400 + 1) * 86400
+
+            # break if now.sunday? || now.saturday?
+            client.say(channel: channel, text: 'そろそろ，お昼ご飯の時間ですよ!!')
+          end
+        end
+      end
+
+    end # class LunchTime
+  end # module Command
+end # module Swimmy
--- lib/swimmy/command/qiita_trend.rb
+++ lib/swimmy/command/qiita_trend.rb
@@ -1,64 +1,100 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+module Swimmy
+  module Command
+    class QiitaTrend < Swimmy::Command::Base
 
-require 'slack-ruby-client'
-require "open-uri"
-require "json"
-require 'dotenv'
-Dotenv.load
+      # per-channel post schedule
+      POST_SCHEDULE = {}
+      QIITA_URI = 'https://qiita.com/api/v2/items?page=1&per_page=5&query=stocks:>100'
 
-post_channel = ENV['POST_CHANNEL']
-
-Slack.configure do |conf|
-  conf.token = ENV["SLACK_API_TOKEN"]
-end
-
-client = Slack::RealTime::Client.new
-
-QIITA_URI = 'https://qiita.com/api/v2/items?page=1&per_page=5&query=stocks:>100'
-
-Thread.new do
-  client.on :hello do
-    array = []
-    loop do
-      t = Time.new
-      unless t.sunday? || t.saturday?
-        if t.hour == 12 && t.min == 00
-          article_str = open("#{QIITA_URI}", &:read)
-          article = JSON.parse(article_str)
-          url = ""
-          if t.monday?
-            array = []
+      command "qtrend" do |client, data, match|
+        case match[:expression]
+        when /(\d+):(\d+)/
+          h, m = $1.to_i, $2.to_i
+          unless 0 <= h && h <= 23 && 0 <= m && m <= 59
+            client.say(channel: data.channel, text: "時刻の指定がおかしいです")
+            break
           end
 
-          count = 0
-          article.each do |q|
-            unless array.include?(q["id"])
-              url += "#{q["url"]} \nいいね数 #{q["likes_count"]}\n"
-              count += 1
-              array.push(q["id"])
-              if count == 3
-                break
-              end
-            end
-          end
+          now = Time.now
+          nxt = Time.new(now.year, now.month, now.day, h, m)
+          nxt += 86400 if nxt <= now
+          POST_SCHEDULE[data.channel] = nxt
+          client.say(channel: data.channel, text: "<##{data.channel}> に #{h}時#{m}分に通知します．")
 
-          if url == ""
-            client.message channel: post_channel, text: "トレンド記事はありません"
-          else
-            client.message channel: post_channel, text: "本日のおすすめ記事です!\n#{url}"
+        when "off"
+          POST_SCHEDULE.delete(data.channel)
+          client.say(channel: data.channel, text: "<##{data.channel}> には通知しません．")
+
+        when "now"
+          qiita = WorkHorse.new(QIITA_URI)
+          qiita.fetch
+          client.say(channel: data.channel, text: qiita.message)
+
+        else
+          client.say(channel: data.channel, text: help_message("qtrend"))
+        end
+      end
+
+      help do
+        title "qtrend"
+        desc "Qiita トレンドを定期的にお知らせします．"
+        long_desc "qtrend HH:MM - 毎日平日 HH:MM 頃，このチャンネルに知らせます．\n" +
+                  "qtrend off - お知らせをやめます．\n" +
+                  "qtrend now - 一度だけすぐに通知します．\n"
+      end
+
+      tick do |client, data|
+        puts "Qiita Trend..."
+        now = Time.new
+        message = nil
+
+        POST_SCHEDULE.each do |channel, time|
+          puts "Qiita Trend Time #{channel} #{time}"
+
+          if time <= now
+            puts "Qiita Trend  Sending message..."
+            # slide schedule to the next HH:MM
+            POST_SCHEDULE[channel] += ((now - time).to_i / 86400 + 1) * 86400
+            # break if now.sunday? || now.saturday?
+            qiita = WorkHorse.new(QIITA_URI)
+            qiita.fetch
+            message ||= qiita.message
+            client.say(channel: data.channel, text: message)
           end
         end
-      else
-        array = []
       end
-      sleep 60
-    end
 
-  end
+      ################################################################
+      ### private inner class
 
-  client.on :closed do
-    client.start!
-  end
+      class WorkHorse
+        require "open-uri"
+        require "json"
 
-  client.start!
-end
+        def initialize(qiita_uri)
+          @qiita_uri = qiita_uri
+        end
+
+        def fetch
+          hash = {}
+          JSON.parse(open(@qiita_uri, &:read)).each do |q|
+            hash[q["id"]] = q
+          end
+          @articles = hash.values
+        end
+
+        def message(max_articles = 3)
+          if @articles.empty?
+            ""
+          else
+            @articles.sort {|a,b| b['likes_count'] <=> a['likes_count'] }
+              .take(max_articles)
+              .map {|q| "#{q['url']} (likes: #{q['likes_count']})"}.join("\n")
+          end
+        end
+      end
+      private_constant :WorkHorse
+
+    end # class QiitaTrend
+  end # module Command
+end # module Swimmy
