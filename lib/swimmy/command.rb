54bef965a2278b3c3fe8b7a071694927c53eac50
Yoshinari Nomura <nom@nomlab.org>
2019-02-22 10:57:52 +0900

    Simplify bootup procedure

--- lib/swimmy/command.rb
+++ lib/swimmy/command.rb
@@ -1,16 +1,11 @@
 module Swimmy
   module Command
     dir = File.dirname(__FILE__) + "/command"
-    
-    require "#{dir}/issue_operation"
-    require "#{dir}/lunch_time"
-    require "#{dir}/rain_information"
-    require "#{dir}/restaurant_information"
-    require "#{dir}/route"
-    require "#{dir}/poll"
-    require "#{dir}/qiita_trend"
-    require "#{dir}/photo_upload"
-    require "#{dir}/lottery"
-    require "#{dir}/reminder"
+
+    require "#{dir}/base.rb"
+
+    Dir.glob(File.expand_path("*.rb", dir)).each do |rb|
+      require rb
+    end
   end
 end
