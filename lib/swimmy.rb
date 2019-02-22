d8b8c41a9985795c01476301fe13c027f90b37ef
Yoshinari Nomura <nom@nomlab.org>
2019-02-22 10:58:34 +0900

    Update requires and Gemfile.lock

--- lib/swimmy.rb
+++ lib/swimmy.rb
@@ -2,7 +2,6 @@ module Swimmy
   dir = File.dirname(__FILE__) + "/swimmy"
 
   autoload :App,                  "#{dir}/app.rb"
-  autoload :Command,              "#{dir}/command.rb"
-  autoload :Web,                  "#{dir}/web.rb"
   autoload :VERSION,              "#{dir}/version.rb"
+  require "#{dir}/command.rb"
 end
