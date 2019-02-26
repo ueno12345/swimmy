688b6dd7997ad985abdd17a3de0c76c53821340c
Yoshinari Nomura <nom@nomlab.org>
2019-02-26 12:43:40 +0900

    Refacoring

--- lib/swimmy/command/photo_upload.rb
+++ lib/swimmy/command/photo_upload.rb
@@ -1,114 +1,64 @@
-# coding: utf-8
 # Ryota Nishi / nomlab
 # This is a part of https://github.com/nomlab/swimmy
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'slack-ruby-bot'
-require 'base'
 require 'json'
 require 'uri'
 require 'net/https'
-
-# swimmy/exe/google_photo_auth を実行することで swimmy/config/google-photo-token.json が作成される
-TOKEN_FILE_PATH = "config/google-photo-token.json"
-
-if File.exist?("config/credentials.json")
-  credentials = JSON.load(File.open("config/credentials.json"))
-
-  CLIENT_ID = credentials["installed"]["client_id"]
-  CLIENT_SECRET = credentials["installed"]["client_secret"]
-end
+require 'pp'
 
 module Swimmy
   module Command
-    class PhotoUploadBot < SlackRubyBot::Server
-      # file_shared イベントが発生すると処理を実行
-      on 'file_shared' do |client, data|
-        # refresh_token を用いて access_token を更新
-        # FIXME: 毎回 access_token の更新を行っている．有効期限を確認して更新するかを判断したほうがよい．
-        token = JSON.load(File.open(TOKEN_FILE_PATH))
-        refresh_token =token ["refresh_token"]
-        request = { :refresh_token => refresh_token, :client_id => CLIENT_ID, :client_secret => CLIENT_SECRET, :grant_type => "refresh_token" }
-        uri = URI.parse("https://www.googleapis.com/oauth2/v4/token")
-        res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          http.post(uri.request_uri, request.to_json, { "Content-Type" => "application/json" })
-        end
-        access_token = JSON.parse(res.body)["access_token"]
-        token["access_token"] = access_token
-        File.open("config/google-photo-token.json", "w") do |f| 
-          f.puts token.to_json
-        end
+    class PhotoUploadBot < Base
+      # https://console.developers.google.com/apis/dashboard
+      # からダウンロードして置いておく
+      GOOGLE_CREDENTIAL_APTH = "config/credentials.json"
 
-        # Slack から画像をダウンロードし，GooglePhoto にアップロード
-        Thread.new do
-          filename, file = SlackFileDownloader.new(ENV['SLACK_API_TOKEN']).download(data.file_id)
-          if file != nil
-            result = GooglePhotoUploader.new(access_token).upload(file, filename)
+      # swimmy/exe/google_photo_auth を実行することで
+      # swimmy/config/google-photo-token.json が作成される
+      GOOGLE_TOKEN_PATH = "config/google-photo-token.json"
 
-            # 結果の送信
-            if result["status"]["message"] == "OK"
-              url = result["mediaItem"]["productUrl"]
-              filename = result["mediaItem"]["filename"]
-              client.say(channel: data.channel_id, text: "Successfully uploaded: <#{url}|#{filename}>")
-            else
-              client.say(channel: data.channel_id, text: "Failed to upload image.")
+      if File.exist?(GOOGLE_CREDENTIAL_APTH)
+        credentials = JSON.load(File.open(GOOGLE_CREDENTIAL_APTH))
+        CLIENT_ID = credentials["installed"]["client_id"]
+        CLIENT_SECRET = credentials["installed"]["client_secret"]
+      end
+
+      on 'message' do |client, data|
+        if data.files
+          data.files.each do |file|
+            next unless ["jpg", "png"].include?(file.filetype)
+            client.say(channel: data.channel, text: "Google Photos にアップロード中 (#{file.name})...")
+
+            Thread.new do
+              begin
+                blob = SlackFileDownloader.new(ENV["SLACK_API_TOKEN"]).fetch(file.url_private_download)
+                url = GooglePhotosUploader.new(GOOGLE_TOKEN_PATH).upload(blob, file.name, data.text)
+                client.say(channel: data.channel, text: "アップロード完了 #{url}")
+              end
             end
           end
         end
-      end
-    end
+      end # on message
+    end # class PhotoUploadBot
 
-    class SlackFileDownloader
-      def initialize(slack_token)
-        @base_url = "https://slack.com/api/files.info"
-        @token = slack_token
-        @allow_filetype = ["jpg", "png"]
+    class GooglePhotosUploader
+      def initialize(google_token_path)
+        @access_token = update_token(google_token_path)
       end
 
-      def download(file_id)
-        # file_id からファイルの情報を取得
-        url = URI.parse(@base_url + "?token=" + @token + "&file=" + file_id)
-        res = Net::HTTP.start(url.host, url.port, use_ssl: true) do |http|
-          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          http.get(url)
-        end
-        file_info = JSON.parse(res.body)
-
-        # 上で取得した res に含まれる url_private の URL からファイルをダウンロード
-        if @allow_filetype.include?(file_info["file"]["filetype"])
-          file_url = URI.parse(file_info["file"]["url_private"])
-          filename = file_info["file"]["name"]
-          req = Net::HTTP::Get.new(file_url.request_uri)
-          req['Authorization'] = "Bearer #{@token}"
-          file = Net::HTTP.start(file_url.host, file_url.port, use_ssl: true) do |http|
-            http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-            http.request(req)
-          end
-          
-          return filename, file.body
-        else
-          # filetype が "jpg"，"png"以外なら nilを返す
-          return nil, nil
-        end
-      end
-    end
-
-    class GooglePhotoUploader
-      def initialize(access_token)
-        @token = access_token
+      def upload(file, filename, comment)
+        # 画像データをアップロード
         @upload_url = "https://photoslibrary.googleapis.com/v1/uploads"
         @mkmedia_url = "https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate"
-      end
 
-      ## GooglePhoto へのアップロードには2段階の処理が必要
-      #  1. @upload_url に画像データを POST
-      #  2. 1のレスポンスに含まれる upload_token を使用して，メディアアイテムの作成を行う
-      def upload(file, filename)
-        # 画像データをアップロード
-        header =  {  "Authorization" => "Bearer #{@token}", "Content-Type" => "application/octet-stream", "X-Goog-Upload-Protocol" => "raw", "X-Goog-Upload-File-Name" =>  filename }
-        uri = URI.parse(@upload_url)        
+        header =  {
+          "Authorization" => "Bearer #{@access_token}",
+          "Content-Type" => "application/octet-stream",
+          "X-Goog-Upload-Protocol" => "raw",
+          "X-Goog-Upload-File-Name" =>  filename
+        }
+
+        uri = URI.parse(@upload_url)
         res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
           http.post(uri.request_uri, file, header)
@@ -116,16 +66,75 @@ module Swimmy
         upload_token = res.body
 
         # メディアアイテムの作成
-        header =  {  "Authorization" => "Bearer #{@token}", "Content-Type" => "application/json" }
-        req = { :newMediaItems => { :simpleMediaItem => { :uploadToken => upload_token } }}
-        uri = URI.parse(@mkmedia_url)        
+        header =  {
+          "Authorization" => "Bearer #{@access_token}",
+          "Content-Type" => "application/json"
+        }
+        req = {:newMediaItems => {:simpleMediaItem => {:uploadToken => upload_token}}}
+        uri = URI.parse(@mkmedia_url)
         res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
           http.verify_mode = OpenSSL::SSL::VERIFY_NONE
           http.post(uri.request_uri, req.to_json, header)
         end
 
-        return JSON.parse(res.body)["newMediaItemResults"][0]
+        result = JSON.parse(res.body)["newMediaItemResults"][0]
+
+        if result["status"]["message"] == "OK"
+          url = result["mediaItem"]["productUrl"]
+          filename = result["mediaItem"]["filename"]
+          "<#{url}|#{filename}>"
+        else
+          nil
+        end
       end
-    end
-  end
-end
+
+      private
+
+      def update_token(google_token_path)
+        # refresh_token を用いて access_token を更新FIXME: 毎回
+        # access_token の更新を行っている．有効期限を確認して更新する
+        # かを判断したほうがよい．
+        token = JSON.load(File.open(google_token_path))
+        refresh_token =token ["refresh_token"]
+        request = {:refresh_token => refresh_token,
+                   :client_id => Swimmy::Command::PhotoUploadBot::CLIENT_ID,
+                   :client_secret => Swimmy::Command::PhotoUploadBot::CLIENT_SECRET,
+                   :grant_type => "refresh_token" }
+
+        uri = URI.parse("https://www.googleapis.com/oauth2/v4/token")
+        res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          http.post(uri.request_uri, request.to_json, {"Content-Type" => "application/json"})
+        end
+        access_token = JSON.parse(res.body)["access_token"]
+
+        token["access_token"] = access_token
+        File.open(google_token_path, "w") do |f|
+          f.puts token.to_json
+        end
+
+        return access_token
+      end
+    end # class GooglePhotosUploader
+
+    class SlackFileDownloader
+      def initialize(api_token)
+        @api_token = api_token
+      end
+
+      def fetch(url_private_download)
+        uri = URI.parse(url_private_download)
+        req = Net::HTTP::Get.new(uri)
+        req['Authorization'] = "Bearer #{@api_token}"
+
+        file = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+          http.request(req)
+        end
+
+        return file.body
+      end
+    end # class SlackFileDownloader
+
+  end # module Command
+end # module Swimmy
